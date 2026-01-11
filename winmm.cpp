3e67046e9bb6d738960ca91dd516954cf220150c
maniman303 <maniman303@gmail.com>
2026-01-11 17:49:20 +0100

    Small refactor

--- winmm.cpp
+++ winmm.cpp
@@ -446,15 +446,17 @@ DWORD WINAPI Load(LPVOID lpParam) {
 	logger::info("Executable name: '" + procFilename + "'.");
 	logger::info("Executable full path: '" + procFullPathString + "'.");
 
+	if (!minhook_facade::init())
+	{
+		return 0;
+	}
+
 	if (to_lower(procFilename).compare("fallout4.exe") == 0 || to_lower(procFilename).compare("fallout4vr.exe") == 0)
 	{
-		if (minhook_facade::init())
-		{
-			ini_workaround::init();
-
-			minhook_facade::enable();
-		}
+		ini_workaround::init();
 	}
+
+	minhook_facade::enable();
 
 	return 0;
 }
